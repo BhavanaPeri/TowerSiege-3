3f7ac3fc0d3ad5765940107bd0fb3f090a6051f4
Bhavana Peri <64651468+BhavanaPeri@users.noreply.github.com>
2020-08-24 08:25:28 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -11,6 +11,7 @@ class Box{
         this.width = width;
         this.height = height;
         this.image=loadImage("cupp.png");
+        this.Visibility = 255;
         this.body = Bodies.rectangle(x, y, width, height, options);
         World.add(world,this.body);
     }
@@ -28,18 +29,18 @@ class Box{
         else{
             World.remove(world,this.body);
         push();
-        this.visibility= this.visibility-5;
+        this.Visibility= this.Visibility-5;
         translate (this.body.position.x,this.body.position.y);
-        tint(255,this.visibility);   
+        tint(255,this.Visibility);   
         imageMode(CENTER)
         image(this.image,0,0,this.width,this.height);
         pop();
         }
-    };
+    }
     
     Score(){
-        if (this.visiblity<0 && this.visiblity > -105){
-          score++;
+        if (this.Visibility<=0 && this.Visibility > -105){
+          score++
         }
       }
 
